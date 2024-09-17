67c4cc44a9713bb11740ed399b8efcdc9aab1d76
BBart19 <107633721+BBart19@users.noreply.github.com>
2024-09-17 17:25:13 +0200

    Update waveshare_tof_laser_range_sensor_b.h

--- components/waveshare_tof_laser_range_sensor_b/waveshare_tof_laser_range_sensor_b.h
+++ components/waveshare_tof_laser_range_sensor_b/waveshare_tof_laser_range_sensor_b.h
@@ -2,21 +2,22 @@
 
 #include "esphome/core/component.h"
 #include "esphome/components/sensor/sensor.h"
-#include "esphome/components/uart/uart.h"
 
 namespace esphome {
 namespace waveshare_tof_laser_range_sensor_b {
 
-class WaveshareTOFLaserRangeSensorB : public uart::UARTDevice, public PollingComponent, public sensor::Sensor {
+class WaveshareTOFLaserRangeSensorB : public Component, public sensor::Sensor {
  public:
-  WaveshareTOFLaserRangeSensorB() = default;
+  WaveshareTOFLaserRangeSensorB(int rx_pin, int tx_pin);
 
   void setup() override;
-  void update() override;
+  void loop() override;
   void dump_config() override;
 
  protected:
-  bool verify_checksum(const uint8_t *data, uint8_t len);
+  bool verify_checksum(uint8_t data[], uint8_t len);
+  int rx_pin_;
+  int tx_pin_;
 };
 
 }  // namespace waveshare_tof_laser_range_sensor_b
