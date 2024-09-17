42f64aa509a5475d23f036aba0b3eb8e1913df23
BBart19 <107633721+BBart19@users.noreply.github.com>
2024-09-17 17:24:59 +0200

    Update sensor.py

--- components/waveshare_tof_laser_range_sensor_b/sensor.py
+++ components/waveshare_tof_laser_range_sensor_b/sensor.py
@@ -1,7 +1,10 @@
 import esphome.codegen as cg
 import esphome.config_validation as cv
-from esphome.components import sensor, uart
+from esphome.components import sensor
 from esphome.const import (
+    CONF_ID,
+    CONF_RX_PIN,
+    CONF_TX_PIN,
     STATE_CLASS_MEASUREMENT,
     ICON_ARROW_EXPAND_VERTICAL,
     DEVICE_CLASS_DISTANCE,
@@ -13,24 +16,24 @@ DEPENDENCIES = ["uart"]
 
 waveshare_ns = cg.esphome_ns.namespace("waveshare_tof_laser_range_sensor_b")
 WaveshareTOFLaserRangeSensorB = waveshare_ns.class_(
-    "WaveshareTOFLaserRangeSensorB", sensor.Sensor, cg.PollingComponent, uart.UARTDevice
+    "WaveshareTOFLaserRangeSensorB", sensor.Sensor, cg.Component
 )
 
-CONFIG_SCHEMA = (
-    sensor.sensor_schema(
-        unit_of_measurement=UNIT_MILLIMETER,
-        icon=ICON_ARROW_EXPAND_VERTICAL,
-        accuracy_decimals=0,
-        state_class=STATE_CLASS_MEASUREMENT,
-        device_class=DEVICE_CLASS_DISTANCE,
-    )
-    .extend(cv.polling_component_schema("1s"))
-    .extend(uart.UART_DEVICE_SCHEMA)
-    .extend({cv.GenerateID(): cv.declare_id(WaveshareTOFLaserRangeSensorB)})
+CONFIG_SCHEMA = sensor.sensor_schema(
+    unit_of_measurement=UNIT_MILLIMETER,
+    icon=ICON_ARROW_EXPAND_VERTICAL,
+    accuracy_decimals=0,
+    state_class=STATE_CLASS_MEASUREMENT,
+    device_class=DEVICE_CLASS_DISTANCE,
+).extend(
+    {
+        cv.GenerateID(): cv.declare_id(WaveshareTOFLaserRangeSensorB),
+        cv.Required(CONF_RX_PIN): cv.int_,
+        cv.Required(CONF_TX_PIN): cv.int_,
+    }
 )
 
 async def to_code(config):
-    var = cg.new_Pvariable(config[cv.GenerateID()])
+    var = cg.new_Pvariable(config[CONF_ID], config[CONF_RX_PIN], config[CONF_TX_PIN])
     await cg.register_component(var, config)
     await sensor.register_sensor(var, config)
-    await uart.register_uart_device(var, config)
